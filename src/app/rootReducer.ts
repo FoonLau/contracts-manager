493324e71b375b2ac3a15b33aa9f05f774d299ac
huan liu <foon@huandeMacBook-Air.local>
2018-11-13 22:27:27 +0800

    feat: Remove redux-form

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form'
 import editorReducer from '../components/ContractEditor/duck';
 import listReducer from '../components/ContractList/duck';
 
 export default combineReducers({
   contracts: listReducer,
-  currentContract: editorReducer,
-  form: formReducer
+  currentContract: editorReducer
 });
